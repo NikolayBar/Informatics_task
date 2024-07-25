81e4a4e713ac3318bd4ad23b78c4a283811f9f9c
NikolayBar <nikolbar@yandex.ru>
2024-07-26 00:00:05 +0300

    add task_1412.py

--- Search_and_sort/task_1412.py
+++ Search_and_sort/task_1412.py
@@ -1,17 +1,15 @@
 """
-9
-3
-1 1 1
-2 2 2
-3 2 3
-
+Для каждого столбца выведите YES, если в нем есть число Х,
+и NO в противном случае. (Каждый ответ с новой строки)
 """
-
-
 X, N = int(input()), int(input())
 
 matrix = [[int(x) for x in input().split()] for _ in range(N)]
 
 for i in range(N):
-    pass
+    res = [matrix[y][i] for y in range(N)]
+    if X in res:
+        print('YES')
+    else:
+        print('NO')
 
