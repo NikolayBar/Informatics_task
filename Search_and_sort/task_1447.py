fc5545df6ecc2e1bdcbf5e1244bbeac36031a15b
NikolayBar <nikolbar@yandex.ru>
2024-07-26 15:45:40 +0300

    add task_1447.py

---
+++ Search_and_sort/task_1447.py
@@ -0,0 +1,14 @@
+input_ = (int(x) for x in input().split())
+
+lst_ = list(input_)[1:]
+
+max_ = max(lst_)
+min_ = min(lst_)
+
+print(lst_)
+
+for i in range(len(lst_)):
+    if lst_[i] == max_:
+        lst_[i] = min_
+
+print(lst_)
